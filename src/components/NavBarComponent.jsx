8bf352fa0c2019c9a69185d09b14f9d09c031aec
Namaummbo <spikkernamaumbo@gmail.com>
2024-10-15 19:26:44 +0200

    removed the other useless components

--- src/components/NavBarComponent.jsx
+++ src/components/NavBarComponent.jsx
@@ -3,36 +3,40 @@ import React from "react";
 const NavBarComponent = () => {
   const linkStyle =
     "text-white hover:text-gray-300 px-2 sm:px-4 py-2 text-base sm:text-lg font-semibold font-paragraph transition duration-300";
-    return (
-      <nav className="bg-black sticky top-0 z-50">
-        <div className="flex flex-col sm:flex-row justify-between items-center relative z-10 p-2">
-          <div className="logo mb-2 sm:mb-0 cursor-pointer" onClick={() => window.location.href = '/'}>
-            <h1 className="text-2xl sm:text-3xl font-bold text-orange-500 mb-1 font-coiny">
-              <img
-                src="/image.png"
-                alt="ZBC Logo"
-                className=" w-16 h-16 sm:w-18 sm:h-18 inline-block ml-2-mt-2-mb-2"
-                loading="eager"
-                width="80"
-                height="80"
-              />
-            </h1>
-          </div>
-          <div className="links flex flex-wrap justify-center">
-            {[
-              { href: "/about", text: "About" },
-              { href: "/ministries", text: "Ministries" },
-              { href: "/services", text: "Services" },
-              { href: "/contact", text: "Contact" },
-              { href: "/giving", text: "Giving" }
-            ].map((link) => (
-              <a key={link.href} href={link.href} className={linkStyle}>
-                {link.text}
-              </a>
-            ))}
-          </div>
+  return (
+    <nav className="bg-black sticky top-0 z-50">
+      <div className="flex flex-col sm:flex-row justify-between items-center relative z-10 p-2">
+        <div
+          className="logo mb-2 sm:mb-0 cursor-pointer"
+          onClick={() => (window.location.href = "/")}
+        >
+          <h1 className="text-2xl sm:text-3xl font-bold text-orange-500 mb-1 font-coiny">
+            <img
+              src="/image.png"
+              alt="ZBC Logo"
+              className=" w-16 h-16 sm:w-18 sm:h-18 inline-block ml-2-mt-2-mb-2"
+              loading="eager"
+              width="80"
+              height="80"
+            />
+          </h1>
         </div>
-      </nav>
-    );
-}
+        <div className="links flex flex-wrap justify-center">
+          {[
+            { href: "/about", text: "About" },
+            { href: "/ministries", text: "Ministries" },
+            { href: "/sermons", text: "sermons" },
+            { href: "/contact", text: "Contact" },
+            { href: "/events", text: "Events" },
+            { href: "/giving", text: "Giving" },
+          ].map((link) => (
+            <a key={link.href} href={link.href} className={linkStyle}>
+              {link.text}
+            </a>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
+};
 export default NavBarComponent;
--- src/pages/ministries-page/MinistriesPage.jsx
+++ src/pages/ministries-page/MinistriesPage.jsx
@@ -24,13 +24,34 @@ const ParallaxScroll = () => {
     <div className="relative bg-black">
       <NavBarComponent />
 
+      <div className="bg-gradient-to-r from-[#160b14] to-black flex flex-col items-center justify-center pb-32 ">
+        <motion.h3
+          className="text-6xl text-center font-bold font-coiny text-orange-700 pb-5 pt-24"
+          initial={{ opacity: 0, y: -50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          ZBC Ministries
+        </motion.h3>
+
+        <motion.p 
+        className="w-[50%] text-white font-paragraph text-center"
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.5 }}
+        >
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris.
+        </motion.p>
+      </div>
+
       <motion.div
-        className="h-screen sticky top-0 flex items-center justify-center bg-no-repeat"
+        className="h-screen sticky top-0 flex items-center justify-center bg-no-repeat "
         style={{
           height: "100vh",
           backgroundImage: "url('/bg-5.jpg')",
-          backgroundAttachment: "fixed",
-          backgroundPosition: `center ${scrollPosition * -0.5}px`,
+          backgroundPosition: `center ${scrollPosition * -0.3}px`,
           y: y2,
         }}
       >
@@ -42,8 +63,96 @@ const ParallaxScroll = () => {
             transition={{ duration: 1 }}
           >
             <h3 className="font-bold text-5xl font-coiny w-96">
-              Children Ministryoo
+              Children Ministry
             </h3>
+            <p className="text-gray-600 mt-4 font-paragraph w-[96%] ">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cup
+            </p>
+          </motion.div>
+
+          <motion.div
+            className=" rounded-sm mb-5 shadow-2xl w-[40%] ml-28 mt-10"
+            initial={{ opacity: 0, x: 10 }}
+            whileInView={{ opacity: 1, x: -150 }}
+            transition={{ duration: 1 }}
+          >
+            <img
+              src="/kids.jpg"
+              alt="Children's Ministry"
+              className="w-full h-full shadow-sm rounded-sm"
+            />
+          </motion.div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        className="h-screen bg-cover bg-center bg-fixed bg-no-repeat sticky top-0 flex items-center justify-center"
+        style={{
+          height: "100vh",
+          backgroundImage: "url('/bg-3.jpg')",
+          backgroundPosition: `center ${scrollPosition * -0.2}px`,
+          y: y2,
+        }}
+      >
+        <div className="flex flex-row justify-center">
+          <motion.div
+            className=" rounded-sm mb-5 shadow-2xl w-[40%] mt-10"
+            initial={{ opacity: 0, x: 10 }}
+            whileInView={{ opacity: 1, x: 150 }}
+            transition={{ duration: 1 }}
+          >
+            <img
+              src="/kids.jpg"
+              alt="Children's Ministry"
+              className="w-full h-auto shadow-sm rounded-sm"
+            />
+          </motion.div>
+          <motion.div
+            className="bg-white rounded-sm p-6 shadow-2xl w-[40%] ml-28"
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <h3 className="font-bold text-5xl font-coiny w-96">
+              Youth Ministry
+            </h3>
+            <p className="text-gray-600 mt-4 font-paragraph w-[96%] ">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cup
+            </p>
+          </motion.div>
+        </div>
+      </motion.div>
+
+      <motion.div
+        className="bg-cover bg-center bg-fixed bg-no-repeat sticky top-0 flex items-center justify-center"
+        style={{
+          height: "100vh",
+          backgroundImage: "url('/bg-5.jpg')",
+          y: y2,
+        }}
+      >
+        <div className="flex flex-row justify-center">
+          <motion.div
+            className="bg-white rounded-sm p-6 shadow-2xl w-[40%] ml-28"
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1 }}
+          >
+            <h3 className="font-bold text-5xl font-coiny w-96">Men Ministry</h3>
             <p className="text-gray-600 mt-4 font-paragraph w-[96%] ">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
@@ -99,7 +208,7 @@ const ParallaxScroll = () => {
             transition={{ duration: 1 }}
           >
             <h3 className="font-bold text-5xl font-coiny w-96">
-              Children Ministry
+              Women Ministry
             </h3>
             <p className="text-gray-600 mt-4 font-paragraph w-[96%] ">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -115,50 +224,7 @@ const ParallaxScroll = () => {
         </div>
       </motion.div>
 
-      <motion.div
-        className="h-screen bg-cover bg-center bg-fixed bg-no-repeat sticky top-0 flex items-center justify-center"
-        style={{
-          height: "100vh",
-          backgroundImage: "url('/bg-5.jpg')",
-          y: y2,
-        }}
-      >
-        <div className="flex flex-row justify-center">
-          <motion.div
-            className="bg-white rounded-sm p-6 shadow-2xl w-[40%] ml-28"
-            initial={{ opacity: 0, y: 50 }}
-            whileInView={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1 }}
-          >
-            <h3 className="font-bold text-5xl font-coiny w-96">
-              Children Ministry
-            </h3>
-            <p className="text-gray-600 mt-4 font-paragraph w-[96%] ">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cup
-            </p>
-          </motion.div>
-
-          <motion.div
-            className=" rounded-sm mb-5 shadow-2xl w-[40%] ml-28 mt-10"
-            initial={{ opacity: 0, x: 10 }}
-            whileInView={{ opacity: 1, x: -150 }}
-            transition={{ duration: 1 }}
-          >
-            <img
-              src="/kids.jpg"
-              alt="Children's Ministry"
-              className="w-full h-auto shadow-sm rounded-sm"
-            />
-          </motion.div>
-        </div>
-      </motion.div>
+      <h2 className="text-white border mt-[-20%]">hi</h2>
     </div>
   );
 };
