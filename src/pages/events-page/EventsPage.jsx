c0976d5b9b8600ac60688bc06884c16ed901e88e
Namaummbo <spikkernamaumbo@gmail.com>
2024-11-22 23:32:28 +0200

    optimized for build

--- src/pages/events-page/EventsPage.jsx
+++ src/pages/events-page/EventsPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBarComponent from "../../components/NavBarComponent";
-import { motion } from "framer-motion";
+// import { motion } from "framer-motion";
 import EventCardComponent from "../../components/EventCardComponent";
 import { Button } from "flowbite-react";
 const EventsPage = () => {
