839aa1da5d1bdd139a701cad4665468c2dcb563d
Namaumbo <daelo.namaumbo@gmail.com>
2025-01-23 12:16:13 +0200

    rf

--- src/pages/about-page/AboutPage.jsx
+++ src/pages/about-page/AboutPage.jsx
@@ -188,25 +188,24 @@ const AboutPage = () => {
       </div>
 
       <div className="max-w-6xl mx-auto px-4">
-        <div className="w-full  md:mb-0 p-8">
+        <div className="w-full  md:mb-0 p-8 ">
           <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold text-semi_heading_color mb-4 md:pr-8 font-heading text-center">
-            Our <span className="text-brand_color font-welcome">Identity,</span>{" "}
-            Work and
-            <span className="relative z-[1] inline-block ml-1 text-white px-4 py-2">
-              Values
-              <span
-                className="absolute bg-brand_color inset-0 ml-2 skew-x-[-15deg]"
-                aria-hidden="true"
-                style={{
-                  zIndex: -1,
-                  transformOrigin: "right",
-                }}
-              ></span>
-            </span>
+            Our <span className="">Identity,</span> Work and Values
           </h1>
+          <div class="mt-2 flex justify-end ">
+            <span class="relative mt-3 whitespace-nowrap text-blue-600">
+              <svg
+                aria-hidden="true"
+                viewBox="0 0 418 42"
+                class="absolute top-[-2.3rem] float-right right-[13rem] h-[2.5em] w-fit fill-brand_color"
+                preserveAspectRatio="none"
+              >
+                <path d="M203.371.916c-26.013-2.078-76.686 1.963-124.73 9.946L67.3 12.749C35.421 18.062 18.2 21.766 6.004 25.934 1.244 27.561.828 27.778.874 28.61c.07 1.214.828 1.121 9.595-1.176 9.072-2.377 17.15-3.92 39.246-7.496C123.565 7.986 157.869 4.492 195.942 5.046c7.461.108 19.25 1.696 19.17 2.582-.107 1.183-7.874 4.31-25.75 10.366-21.992 7.45-35.43 12.534-36.701 13.884-2.173 2.308-.202 4.407 4.442 4.734 2.654.187 3.263.157 15.593-.78 35.401-2.686 57.944-3.488 88.365-3.143 46.327.526 75.721 2.23 130.788 7.584 19.787 1.924 20.814 1.98 24.557 1.332l.066-.011c1.201-.203 1.53-1.825.399-2.335-2.911-1.31-4.893-1.604-22.048-3.261-57.509-5.556-87.871-7.36-132.059-7.842-23.239-.254-33.617-.116-50.627.674-11.629.54-42.371 2.494-46.696 2.967-2.359.259 8.133-3.625 26.504-9.81 23.239-7.825 27.934-10.149 28.304-14.005.417-4.348-3.529-6-16.878-7.066Z"></path>
+              </svg>
+            </span>
+          </div>
         </div>
 
-        <div className="bg-[#eef5fc] h-3 "></div>
         <div className="grid md:grid-cols-2 gap-8 py-12 ">
           <div className="bg-gradient-to-br from-white to-gray-50 p-8 rounded-2xl   transition-all duration-300 transform  border border-gray-100">
             <div className="flex items-center mb-6">
