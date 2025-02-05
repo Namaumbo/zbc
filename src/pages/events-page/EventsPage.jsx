ba6a666df99bdbfc252a6d7f4c40856086c885b0
Namaumbo <daelo.namaumbo@gmail.com>
2025-02-05 15:17:22 +0200

    fix on no response

--- src/pages/events-page/EventsPage.jsx
+++ src/pages/events-page/EventsPage.jsx
@@ -21,7 +21,11 @@ const EventsPage = () => {
         );
         setEvents(response.data.data);
       } catch (err) {
+        setEvents([]);
+
         console.log(err);
+      } finally {
+        // setEvents([]);
       }
     };
 
@@ -60,12 +64,12 @@ const EventsPage = () => {
             Upcoming Events
           </h2>
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-            {events.map((event,index) => (
+            {events.map((event, index) => (
               <div
                 key={index}
                 className="transform transition-transform hover:scale-105"
               >
-                <EventCardComponent  data={event}/>
+                <EventCardComponent data={event} />
               </div>
             ))}
           </div>
